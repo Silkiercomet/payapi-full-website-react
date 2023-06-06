978d337afdf3673d4dbbeda5482c801a7c840452
luis Colina <colina.1990lz@gmail.com>
2023-06-06 08:59:04 -0400

    structure of the application is finished

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+/*
+- create the main structure of the app, including the pages, routing, and components place  
+- style the home page and his components
+- style the about page and his components
+- style the pricing page and his components
+- style the contact page and his components
+- add the form funtionalitie to the contact page
+- refactor the app
+*/
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
